1caf925224987bb9ce92dc8ada3b4304b913eb15
Seulgi <jusanha17@gmail.com>
2025-11-20 14:40:36 +0900

    feat: Update project to latest instructor version

--- src/main/java/com/kt/ShoppingApplication.java
+++ src/main/java/com/kt/ShoppingApplication.java
@@ -1,9 +1,12 @@
-package java.com.kt;
+package com.kt;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
+// @ServletComponentScan
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class ShoppingApplication {
 
 	public static void main(String[] args) {
