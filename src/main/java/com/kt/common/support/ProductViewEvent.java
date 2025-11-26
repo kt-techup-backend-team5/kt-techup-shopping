5dac6f83cae0297a8c18f9eae38686f196affc7e
YE_desktop <12202340@inha.edu>
2025-11-26 16:41:10 +0900

    feat: 조회수 집계 및 조회 로직 구현

---
+++ src/main/java/com/kt/common/support/ProductViewEvent.java
@@ -0,0 +1,4 @@
+package com.kt.common.support;
+
+public record ProductViewEvent(Long id) {
+}
--- src/main/java/com/kt/controller/product/ProductController.java
+++ src/main/java/com/kt/controller/product/ProductController.java
@@ -1,5 +1,6 @@
 package com.kt.controller.product;
 
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.data.domain.Page;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -9,9 +10,11 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.kt.common.request.Paging;
 import com.kt.common.response.ApiResult;
+import com.kt.common.support.ProductViewEvent;
 import com.kt.common.support.SwaggerAssistance;
 import com.kt.dto.product.ProductResponse;
 import com.kt.service.ProductService;
+import com.kt.service.RedisService;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
@@ -25,6 +28,8 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class ProductController extends SwaggerAssistance {
 	private final ProductService productService;
+	private final RedisService redisService;
+	private final ApplicationEventPublisher applicationEventPublisher;
 
 	@Operation(summary = "상품 검색 및 조회", description = "활성화, 품절 상태인 전체 상품 목록을 검색 및 조회합니다. 키워드를 입력하지 않으면 전체 상품이 조회됩니다.",
 			parameters = {
@@ -44,12 +49,19 @@ public class ProductController extends SwaggerAssistance {
 		return ApiResult.ok(search);
 	}
 
-	@Operation(summary = "상품 상세 조회", description = "상품의 상세 정보를 조회합니다.")
+	@Operation(summary = "상품 상세 조회", description = "상품의 상세 정보를 조회합니다.", parameters = {
+			@Parameter(name = "keyword", description = "검색 키워드", example = ""),
+			@Parameter(name = "page", description = "페이지 번호", example = "1"),
+			@Parameter(name = "size", description = "페이지 크기", example = "10")
+	})
 	@GetMapping("/{id}")
 	@SecurityRequirement(name = "Bearer Authentication")
 	public ApiResult<ProductResponse.Detail> detail(@PathVariable Long id) {
-		var product = productService.detail(id);
+		applicationEventPublisher.publishEvent(new ProductViewEvent(id));
 
-		return ApiResult.ok(ProductResponse.Detail.of(product));
+		var product = productService.detail(id);
+		var viewCount = redisService.getViewCount(id);
+
+		return ApiResult.ok(ProductResponse.Detail.of(product, viewCount));
 	}
 }
