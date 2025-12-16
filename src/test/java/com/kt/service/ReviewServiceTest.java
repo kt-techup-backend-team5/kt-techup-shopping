52cac447ec4d8d2449801c0a604dda58d9097766
Seulgi <jusanha17@gmail.com>
2025-12-16 19:22:48 +0900

    @ActiveProfiles("test") 추가

--- src/test/java/com/kt/service/ReviewServiceTest.java
+++ src/test/java/com/kt/service/ReviewServiceTest.java
@@ -29,10 +29,12 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.transaction.annotation.Transactional;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @Transactional
+@ActiveProfiles("test")
 class ReviewServiceTest {
 	@Autowired
 	private ReviewService reviewService;
