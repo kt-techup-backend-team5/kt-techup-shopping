6be8cdfd8b698827fac10e13b5f5feefdb96e9dc
Seulgi <117719441+SeulGi0117@users.noreply.github.com>
2025-12-29 17:38:15 +0900

    hotfix(test): 컨트롤러 테스트 실패 원인인 JwtFilter mock 제거 (#113)
    - AdminProductControllerTest, AdminQuestionControllerTest에서 @MockitoBean JwtFilter 제거
    - Mock 필터가 filterChain.doFilter() 호출하지 않아 요청이 컨트롤러에 도달하지 못하는 문제 해결
    - @WithMockCustomUser가 이미 인증을 처리하므로 JwtFilter mock 불필요

--- src/test/java/com/kt/controller/payment/PaymentControllerTest.java
+++ src/test/java/com/kt/controller/payment/PaymentControllerTest.java
@@ -21,7 +21,6 @@ import com.kt.domain.payment.PaymentType;
 import com.kt.dto.payment.PaymentRequest;
 import com.kt.repository.payment.PaymentTypeRepository;
 import com.kt.repository.user.UserRepository;
-import com.kt.security.JwtFilter;
 import com.kt.security.JwtService;
 import com.kt.security.WithMockCustomUser;
 import com.kt.service.PaymentService;
@@ -45,9 +44,6 @@ class PaymentControllerTest {
 
 	@MockitoBean
 	private JwtService jwtService;
-
-	@MockitoBean
-	private JwtFilter jwtFilter;
 
 	@MockitoBean
 	private UserRepository userRepository;
